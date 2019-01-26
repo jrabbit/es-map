c97f7d9e466dfe708ccc58dd1d73dfcd98678483
Jack Laxson <jackjrabbit@gmail.com>
2019-01-25 21:31:05 -0500

    add client checkin befroe fetch, make apis actually work sorta

--- map/views.py
+++ map/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import generics, viewsets
 # Create your views here.
 from .serializers import *
 from .models import *
 
-class ChapterAPIView(generics.ListAPIView):
+class ChapterAPIView(viewsets.ReadOnlyModelViewSet):
     queryset = Chapter.objects.all()
     serializer_class = ChapterSerializer
 
-class EventAPIView(generics.ListAPIView):
+class EventAPIView(viewsets.ReadOnlyModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
